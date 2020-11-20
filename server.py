aacea40e1aa6692a880d15886e4b0779ed4bc72e
Hanjeong Lee <gkswjdzz@naver.com>
2020-11-20 04:06:45 +0000

    change shape of requestbody

--- server.py
+++ server.py
@@ -1,30 +1,30 @@
 from flask import Flask, request, jsonify
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoModelForCausalLM
+import torch
 import math
 import os
-import time
 
 
 MODEL_NAME=os.environ['MODEL_NAME']
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 model = AutoModelForCausalLM.from_pretrained(MODEL_NAME, return_dict=True)
 model.to(device='cuda')
 
 app = Flask(__name__)
 
 
-def score(raw_text):
-    encoded_text = tokenizer(raw_text, return_tensors='pt').to(device='cuda')
-    output = model(**encoded_text, labels=encoded_text['input_ids'])
+def score(encoded_text):
+    encoded_text = torch.cuda.LongTensor(encoded_text)
+    output = model(input_ids=encoded_text, labels=encoded_text)
     return math.exp(output.loss)
 
 
 @app.route('/evaluate', methods=['POST'])
-def torch():
+def evaluate():
     if request.is_json:
         content = request.get_json()
-        return jsonify({ "score": score(content['context'])}), 200 
-    return jsonify({ "score": -1 }), 400
+        print(content, type(content))
+        return jsonify({"score": score(content)}), 200 
+    return jsonify({"score": -1}), 400
 
 
 if __name__ == "__main__":
